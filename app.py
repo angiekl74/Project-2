794139658f31286705d584c5f9115722cb45224d
Chad Gielow <Chad.Gielow@nike.com>
2020-04-14 18:33:56 -0700

    working flask app.py

--- app.py
+++ app.py
@@ -8,25 +8,23 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+
 #################################################
 # Database Setup
 #################################################
-engine = create_engine('postgresql://localhost:5432/aqiproject')
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/Project2AQI")
+
 Base = automap_base()
 # reflect the table
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-aqi = Base.classes.aqi 
-# Titles = Base.classes.titles (WHAT TABLE NAMES ARE WE GOING TO USE?)
-​
-#################################################
+Project2AQI = Base.classes.boise
+
 # Flask Setup
 #################################################
 app = Flask(__name__)
-​
-​
-#################################################
+
 # Flask Routes
 #################################################
 
@@ -37,27 +35,19 @@ def welcome():
         f"Welcome to the Air Quality Index Data!<br/>"
         f"Available Routes:<br/>"
         f"<a href='/api/v1.0/aqi'> Air Quality Index data</a><br/>"
-        # f"<a href='/api/v1.0/stations'> List of stations </a><br/>"
-        # f"<a href='/api/v1.0/tobs'> Temperature observations of the most active station for the last year of data. </a><br/>"
-        # f"<a href='/api/v1.0/start'> Temperature for greater than and equal to the start date.</a><br/>"
-        # f"<a href='/api/v1.0/start/end'> Temperature for dates between the start and end date inclusive.</a><br/>"
-       
     )
 
 @app.route("/api/v1.0/aqi")   
 def aqi():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-​
-   # Perform a query to retrieve the data
-    aqi_score = session.query(aqi.city, aqi.lat, aqi.lon, aqi.value, aqi.date)
+
+    aqi_score = session.query(Project2AQI.date, Project2AQI.overall_aqi_value, Project2AQI.main_pollutant, Project2AQI.site_name, Project2AQI.site_id, Project2AQI.ozone, Project2AQI.pm25, Project2AQI.no2, Project2AQI.lat, Project2AQI.lon, Project2AQI.city_name, Project2AQI.state_ordinance).all()
     
     session.close()
-​
-    # Create a dictionary from the row data and append to a list of all_aqi results
+
     all_aqi = list(np.ravel(aqi_score))
-    
-​
+
     return jsonify(all_aqi)
 
 if __name__ == '__main__':
