364459958df05bf1df9bda1a8788b4262e44fb28
Daniel Love <daniellove@Daniels-MBP.domain>
2020-04-18 09:09:12 -0700

    toying with pulling from api

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
-# from flask_cors import CORS
+from flask_cors import CORS
 
 
 #################################################
@@ -35,7 +35,7 @@ Indianapolis = Base.classes.indianapolis
 # Flask Setup
 #################################################
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 # Flask Routes
 #################################################
